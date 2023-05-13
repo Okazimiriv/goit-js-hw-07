302a27ba4aa22972d0ec6c50534c6d9a23661477
Oksana Komarnytska <okazimiriv@gmail.com>
2023-05-13 20:21:31 +0200

    ремонт-файл

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,52 +1,114 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// import { supportItems } from './support_ukraie_items';
 
-const galleryItemsContainer = document.querySelector('.gallery');
-const cardsMarkup = createGaleryMarkup(galleryItems);
-let instance;
+// const galleryItemsContainer = document.querySelector('.swiper-wrapper');
+// const fundsMarkup = createFundsMarkup(supportItems);
 
-galleryItemsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
-galleryItemsContainer.addEventListener('click', openGalleryModal);
+// galleryItemsContainer.insertAdjacentHTML('beforeend', createFundsMarkup);
+
+// function numberFounds(arr) {
+//   for (let i = 0; i < arr.length; i += 1) {}
+// }
+// numberFounds(supportItems);
+
+// function createFundsMarkup(supportItems) {
+//   console.log(supportItems.children);
+
+//   return supportItems
+//     .map(({ title, url, img }) => {
+//       return `
+//             <li class="support_gallery_item swiper-slide">
+//               <a class="support_gallery_link link" href="${url}" title="${title}">
+//                 <svg class="support_gallery_link_svg" height="20">
+//                 <use href="${img}"></use>
+//                 </svg>
+//               </a>
+//             </li>
+//             `;
+//     })
+//     .join('');
+// }
+
+// console.log(supportItems);
+
+// const swiper = new Swiper('.mySwiper', {
+//   slidesPerView: 4,
+//   spaceBetween: 20,
+//   direction: 'vertical',
+//   loop: true,
+
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//   },
+// });
 
 
-function createGaleryMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>
-    `;
-    })
-    .join('');
-}
 
-function openGalleryModal(evt) { 
-  const { target } = evt;
-  evt.preventDefault();
-  window.addEventListener('keydown', onEscKeyPress);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(galleryItems);
+
+// const galleryItemsContainer = document.querySelector('.gallery');
+// const cardsMarkup = createGaleryMarkup(galleryItems);
+// let instance;
+
+// galleryItemsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+// galleryItemsContainer.addEventListener('click', openGalleryModal);
+
+
+// function createGaleryMarkup(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+// <li class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </li>
+//     `;
+//     })
+//     .join('');
+// }
+
+// function openGalleryModal(evt) { 
+//   const { target } = evt;
+//   evt.preventDefault();
+//   window.addEventListener('keydown', onEscKeyPress);
   
-const isGalleryImageEl = target.classList.contains('gallery__image');
+// const isGalleryImageEl = target.classList.contains('gallery__image');
 
-  if (!isGalleryImageEl) {
-    return;
-  }
+//   if (!isGalleryImageEl) {
+//     return;
+//   }
 
-const imgDataSource = target.dataset.source
-const currentItem = galleryItems.find(({original}) => original === imgDataSource)  
+// const imgDataSource = target.dataset.source
+// const currentItem = galleryItems.find(({original}) => original === imgDataSource)  
   
-  instance = basicLightbox.create(`
-    <img src="${currentItem.preview}">
-`); 
+//   instance = basicLightbox.create(`
+//     <img src="${currentItem.preview}">
+// `); 
    
   instance.show(); 
 }
--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,65 +1,113 @@
-export const galleryItems = [
+// export const galleryItems = [
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+//     description: 'Hokkaido Flower',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+//     description: 'Container Haulage Freight',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+//     description: 'Aerial Beach View',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+//     description: 'Flower Blooms',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+//     description: 'Alpine Mountains',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+//     description: 'Mountain Lake Sailing',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+//     description: 'Alpine Spring Meadows',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+//     description: 'Nature Landscape',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+//     description: 'Lighthouse Coast Sea',
+//   },
+// ];
+
+export const supportItems = [
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-    description: 'Hokkaido Flower',
+    title: "Save the Children",
+    url: "https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis",
+    img: "./src/images/svg/sprite.svg#safe_children_icon",
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
+    title: "Project HOPE",
+    url: "https://www.projecthope.org/country/ukraine/",
+    img: "./src/images/svg/sprite.svg#project_hope_icon",
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
+    title: "UNITED24",
+    url: "https://u24.gov.ua/uk",
+    img: "./src/images/svg/sprite.svg#united24_icon",
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
+    title: "International Medical Corps",
+    url: "https://internationalmedicalcorps.org/country/ukraine/",
+    img: "./src/images/svg/sprite.svg#international_medical_corp_icon",
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
+    title: "Medicins Sans Frontieres",
+    url: "https://www.msf.org/ukraine",
+    img: "./src/images/svg/sprite.svg#medicines_sans_frontiers_icon",
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
+    title: "RAZOM",
+    url: "https://www.razomforukraine.org/",
+    img: "./src/images/svg/sprite.svg#razom_icon",
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
+    title: "Action against hunger",
+    url: "https://www.actionagainsthunger.org/location/europe/ukraine/",
+    img: "./src/images/svg/sprite.svg#action_against_hunger_icon",
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
+    title: "World vision",
+    url: "https://www.wvi.org/emergencies/ukraine",
+    img: "./src/images/svg/sprite.svg#world_vision_icon",
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
+    title: "Serhiy Prytula Charity Foundation",
+    url: "https://prytulafoundation.org/en",
+    img: "./src/images/svg/sprite.svg#sergiy_prytula_icon",
   },
 ];
